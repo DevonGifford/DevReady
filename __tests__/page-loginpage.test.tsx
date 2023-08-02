c53ce8f3898e33767d4ff7e2e3982ad03e316ddf
DevonGifford <DevonGifford@outlook.com>
2023-08-01 20:00:00 -0600

    testing:  adding unit and integration tests for auth/login-page

---
+++ __tests__/page-loginpage.test.tsx
@@ -0,0 +1,150 @@
+import "@testing-library/jest-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import LoginPage from "@/app/(auth)/(routes)/login/page";
+
+//👇 Renders the Login Page - Client Side Rendered
+
+//- Mock useRouter, useSearchParams, react-hot-toast, ResizeObserver
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+  })),
+  useSearchParams: jest.fn(() => ({
+    get: jest.fn(),
+  })),
+}));
+
+global.ResizeObserver = jest.fn(() => ({
+  // Create a mock for ResizeObserver in test setup
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
+describe.skip("Login Page Rendering Tests", () => {
+  it("renders login page", () => {
+    const { container } = render(<LoginPage />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("renders the header component correctly", () => {
+    render(<LoginPage />);
+
+    expect(screen.getByText("Login to your account")).toBeInTheDocument();
+    expect(screen.getByText(/by improving 1/i)).toBeInTheDocument();
+  });
+
+  it("renders the footer correctly", () => {
+    render(<LoginPage />);
+
+    expect(screen.getByText("Dont have an account?")).toBeInTheDocument();
+    expect(screen.getByText("Back to Home Page")).toBeInTheDocument();
+  });
+});
+
+describe.skip("Login Form Validation Tests", () => {
+  test("case empty email/password renders error messages", async () => {
+    //-Arrange
+    render(<LoginPage />);
+    const emailInput = screen.getByPlaceholderText("email");
+    const passwordInput = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    //-Act
+    await userEvent.click(emailInput);
+    await userEvent.click(passwordInput);
+    await userEvent.click(submitButton);
+
+    //-Assert
+    await waitFor(() => {
+      const passwordRequiredText = screen.getAllByText(/Required/i);
+      expect(passwordRequiredText.length).toBeGreaterThan(1); // Checks both error messages present
+      // 🤔 or assert on a specific element in the array
+      // ? expect(passwordRequiredText[0]).toBeInTheDocument();
+    });
+  });
+
+  test("case short password renders error message", async () => {
+    //-Arrange
+    render(<LoginPage />);
+    const emailInput = screen.getByPlaceholderText("email");
+    const passwordInput = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    //-Act
+    await userEvent.click(emailInput);
+    await userEvent.keyboard("test@gmail.com");
+    await userEvent.click(passwordInput);
+    await userEvent.keyboard("a".repeat(2));
+    await userEvent.click(submitButton);
+
+    //-Assert
+    await waitFor(() => {
+      const shortPasswordError = screen.getByText(
+        /Password must be at least 8 characters/
+      );
+      expect(shortPasswordError).toBeInTheDocument();
+    });
+  });
+
+  test("case incorrect email format renders error message", async () => {
+    //-Arrange
+    render(<LoginPage />);
+    const emailInput = screen.getByPlaceholderText("email");
+    const passwordInput = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    //-Act
+    await userEvent.click(emailInput);
+    await userEvent.keyboard("test");
+    await userEvent.click(passwordInput);
+    await userEvent.keyboard("a".repeat(10));
+    await userEvent.click(submitButton);
+
+    //-Assert
+    await waitFor(() => {
+      expect(screen.getByText(/Invalid email format/i)).toBeInTheDocument();
+    });
+  });
+});
+
+describe.skip("Login Submission Tests", () => {
+  it.skip("successful form submit should have notification and reroute", async () => {
+    render(<LoginPage />);
+    const emailInput = screen.getByPlaceholderText("email");
+    const passwordInput = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    // Fill in correct authentication details and submit
+    await userEvent.type(emailInput, "test2@test.com");
+    await userEvent.type(passwordInput, "test@123");
+    await userEvent.click(submitButton);
+
+    //-Assert
+    setTimeout(() => {
+      expect(screen.getByText("Successfully signed in")).toBeInTheDocument();
+      expect(window.location.pathname).toBe("/dashboard"); // Example of checking for redirection
+    }, 4000);
+  });
+
+  it.skip("unsuccessful form submit should have notification and error messages", async () => {
+    render(<LoginPage />);
+    const emailInput = screen.getByPlaceholderText("email");
+    const passwordInput = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    // Fill in incorrect authentication details and submit
+    await userEvent.type(emailInput, "notrealemail@test.com");
+    await userEvent.type(passwordInput, "notrealpassword");
+    await userEvent.click(submitButton);
+
+    // Assert: Check for error messages
+    setTimeout(() => {
+      expect(
+        screen.getByText("Incorrect credentials, please try again.")
+      ).toBeInTheDocument();
+    });
+  });
+});
