4e696d338764ee7c7edeedc4c15e85f471905d4d
DevonGifford <DevonGifford@outlook.com>
2023-08-07 20:00:00 -0600

    docs: creating skeleton layout and organising project

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Logo from "./components/Logo";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Logo />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/DevonGifford/ZtmReady--PortfolioProject",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://zerotomastery.io/community/developer-community-discord/",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase:
+    "https://github.com/DevonGifford/ZtmReady--PortfolioProject",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "ZTM Ready Documentation 2024",
   },
-}
+};
 
-export default config
+export default config;
