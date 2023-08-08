7f769ee407493de86d4e5dd0ac442c6795bf4f4d
DevonGifford <DevonGifford@outlook.com>
2023-08-07 20:00:00 -0600

    chore: adding embla carousel package via shadcn ui library

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,50 @@
+"use client";
+
+import { useUserContext } from "@/components/providers/UserProvider";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import React from "react";
 
 function MainPage() {
+  const { userProfile } = useUserContext();
   return (
-    <div className="h-full flex flex-col items-center justify-center space-y-4">
-      <h2 className="text-lg font-medium">Welcome to your ZTM Portfolio</h2>
+    <div className="h-full flex flex-col p-20 space-y-4">
+      <h1 className="text-lg font-medium">
+        Welcome Back, {userProfile?.account.username}{" "}
+      </h1>
+
       <h3 className="text-devready-green">Coming Soon</h3>
+
+      <div>
+        <Carousel className="w-full">
+          <CarouselContent className="-ml-1 w-full">
+            {Array.from({ length: 8 }).map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 basis-1/3 md:basis-2/4 xl:basis-1/4"
+              >
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-2xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 }
