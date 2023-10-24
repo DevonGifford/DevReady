318ae673a5489104d152014124faddcfd03f2f71
DevonGifford <DevonGifford@outlook.com>
2023-10-23 20:00:00 -0600

    backend: setting up various types for quiz provider

---
+++ types/quizzSchema.ts
@@ -0,0 +1,22 @@
+export interface QuizQuestions {
+  uuid: number;
+  questionTitle: string;
+  questionDescription?: string;
+  questionAnswer: string;
+  questionDifficulty: number;
+  questionTags: string[];
+}
+
+export interface QuizSchema {
+  uuid: string;
+  title: string;
+  description: string;
+  questions: QuizQuestions[];
+}
+
+export interface QuizResults {
+  quizUuid: string; // References the UUID of the quiz
+  questionUuid: number; // References the UUID of the answered question
+  userAnswer: string; // User's answer to the question
+  isCorrect: boolean; // Indicates whether the user's answer is correct
+}
