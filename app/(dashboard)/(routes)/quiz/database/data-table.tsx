b265741cf2d0896e6ccee033be181d785e59994f
DevonGifford <DevonGifford@outlook.com>
2023-12-17 20:00:00 -0600

    feat: implement sorting and filtering in datatable

--- app/(dashboard)/(routes)/quiz/database/data-table.tsx
+++ app/(dashboard)/(routes)/quiz/database/data-table.tsx
@@ -3,14 +3,18 @@
 import * as React from "react";
 import {
   ColumnDef,
-  flexRender,
+  ColumnFiltersState,
   SortingState,
+  flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -30,6 +34,9 @@ export function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
 
   const table = useReactTable({
     data,
@@ -38,13 +45,26 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
+      columnFilters,
     },
   });
 
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter questions..."
+          value={(table.getColumn("questionTitle")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("questionTitle")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
